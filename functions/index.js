2d4299cf8f9183eec36d7bfc3e7a676d8ccde44e
AndrewH2O <AndrewH2O@users.noreply.github.com>
2020-09-11 23:27:37 +0100

    added express

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@ const functions = require('firebase-functions');
 const express = require("express");
 const cors = require("cors");
 const stripe = require("./stripeApi");
-
 // API
 
 // - App config
